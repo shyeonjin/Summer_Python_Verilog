89320f5188497b076b325a4851274df9a32953cb
hyeonjin Sim <nufxwms@naver.com>
2022-07-04 00:32:20 +0900

    Python week01

--- Python_DOJANG/week01/UNIT_017/pro17_3.py
+++ Python_DOJANG/week01/UNIT_017/pro17_3.py
@@ -2,3 +2,12 @@
 # True 사용 무한 루프
 while True:
     print("Hello")
+
+# 1 사용 무한 루프
+# while 1:
+#    print("Hello")
+
+# 내용문이 있는 문자열은 True로 취급하여 무한 루프로 동작
+
+# while "hello":
+#    print("Hello, world!")
