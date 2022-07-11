4cc9ee6d30ac04e82f31942d064c10cd67c20c3c
hyeonjin Sim <nufxwms@naver.com>
2022-07-11 13:21:47 +0900

    Python week03

--- Python_DOJANG/week03/UNIT_030/pro30_4.py
+++ Python_DOJANG/week03/UNIT_030/pro30_4.py
@@ -6,3 +6,19 @@ def personal_info(name,age,address="비공개"):
 
 personal_info("홍길동",30)
 personal_info("홍길동",30,'서울')
+
+
+# 초기값이 지정된 매개변수의 위치
+# def personal_info(name,addres="비공개",age):
+#     print("이름: ",name)
+#     print("나이: ",age)
+#     print("주소: ",address)
+
+
+# 초기값이 지정된 매개변수는 뒤쪽으로
+def personal_info1(name, age, address='비공개'):
+    pass
+def personal_info2(name, age=0, address='비공개'):
+    pass
+def personal_info3(name="비공개", age=0, address='비공개'):
+    pass
